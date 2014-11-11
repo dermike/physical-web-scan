7953759caca6f5df95db0be06e0769bfbc5bde37
Mikael Jergefelt <mikael.jergefelt@ki.se>
2014-11-11 15:13:52 +0100

    move metadata lookup to module and add native OS notifications

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 var noble = require('noble');
-var jsdom = require('jsdom');
 var chalk = require('chalk');
 var uridecode = require('./uridecode.js');
-
-function getMetaData(url) {
-  jsdom.env(url, function (errors, window) {
-    if (!errors) {
-      console.log(chalk.underline.bgBlue(' ' + window.document.getElementsByTagName('title')[0].innerHTML + ' '));
-      var metatags = window.document.getElementsByTagName('meta');
-      for (var i = 0; i < metatags.length; i++) {
-        if (metatags[i].getAttribute('name') == 'description') {
-          console.log(chalk.gray(metatags[i].getAttribute('content')));
-        }
-      }
-      window.close();
-    }
-    console.log(url);
-  });
-}
+var metadata = require('./metadata.js');
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
@@ -42,7 +26,7 @@ noble.on('discover', function(peripheral) {
   if (serviceData && serviceData.length) {
     for (var i in serviceData) {
       var url = uridecode(serviceData[i].data.toString('hex'));
-      getMetaData(url);
+      metadata(url);
     }
   }
   console.log();
---
+++ metadata.js
@@ -0,0 +1,57 @@
+var jsdom = require('jsdom');
+var chalk = require('chalk');
+var notifier = require('node-notifier');
+
+module.exports = function(url) {
+  var title = '', description = '', favicon = '';
+  jsdom.env(url, function (errors, window) {
+    if (!errors) {
+      if (window.document.getElementsByTagName('title')[0]) {
+        title = window.document.getElementsByTagName('title')[0].innerHTML;
+      } else {
+        title = 'Title not found';
+      }
+
+      var metatags = window.document.getElementsByTagName('meta');
+      if (metatags) {
+        for (var i = 0; i < metatags.length; i++) {
+          if (metatags[i].getAttribute('name') == 'description') {
+            description += metatags[i].getAttribute('content');
+          }
+        }
+      }
+
+      var metalinks = window.document.getElementsByTagName("link");
+      if (metalinks) {
+        for (var i = 0; i < metalinks.length; i++) {
+          if ((metalinks[i].getAttribute("rel") == "icon") || (metalinks[i].getAttribute("rel") == "shortcut icon")) {
+            favicon = metalinks[i].getAttribute("href");
+          }
+        }
+      } else {
+        favicon = '';
+      }
+
+      if (description == '') {
+        description = 'No description';
+      }
+
+      window.close();
+
+      console.log(chalk.underline.bgBlue(' ' + title + ' '));
+      console.log(chalk.gray(description));
+      
+      notifier.notify({
+        title: title,
+        subtitle: url,
+        message: description,
+        icon: './physicalweb.jpg',
+        contentImage: favicon,
+        sound: true,
+        wait: true,
+        open: url
+      });
+    }
+    console.log(url);
+  });
+}
