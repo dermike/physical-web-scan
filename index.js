f35eac5b101c48e29e1a35ff02aefe20b54fa00f
Mikael Jergefelt <mikael.jergefelt@ki.se>
2015-12-27 12:17:54 +0100

    lint and update to basic es2015 syntax

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var noble = require('noble');
-var chalk = require('chalk');
-var urldecode = require('./urldecode.js');
-var metadata = require('./metadata.js');
+'use strict';
+let noble = require('noble'),
+  chalk = require('chalk'),
+  urldecode = require('./urldecode.js'),
+  metadata = require('./metadata.js');
 
-noble.on('stateChange', function(state) {
+noble.on('stateChange', (state) => {
   if (state === 'poweredOn') {
     noble.startScanning(['feaa']);
   } else {
@@ -11,26 +12,25 @@ noble.on('stateChange', function(state) {
   }
 });
 
-noble.on('scanStart', function() {
+noble.on('scanStart', () => {
   console.log(chalk.dim('Scan started...'));
   console.log();
 });
 
-noble.on('scanStop', function() {
+noble.on('scanStop', () => {
   console.log(chalk.dim('Scan stopped...'));
   console.log();
 });
 
-noble.on('discover', function(peripheral) {
-  var serviceData = peripheral.advertisement.serviceData;
+noble.on('discover', (peripheral) => {
+  let serviceData = peripheral.advertisement.serviceData;
   if (serviceData && serviceData.length) {
-    var objects = [];
-    for (var i in serviceData) {
-      
-      // check if Eddystone-URL 
-      if (serviceData[i].data.toString('hex').substr(0,2) === '10') { 
-        var url = urldecode(serviceData[i].data.toString('hex'));
-        objects.push({url: url});
+    let objects = [];
+    for (let i in serviceData) {
+      // check if Eddystone-URL
+      if (serviceData[i].data.toString('hex').substr(0, 2) === '10') {
+        let url = urldecode(serviceData[i].data.toString('hex'));
+        objects.push({'url': url});
       }
     }
     if (objects.length) {
--- metadata.js
+++ metadata.js
@@ -1,78 +1,80 @@
-var http = require('http');
-var chalk = require('chalk');
-var notifier = require('node-notifier');
+'use strict';
+let http = require('http'),
+  chalk = require('chalk'),
+  notifier = require('node-notifier');
 
-module.exports = function(urldata) {
-  var urls = JSON.stringify({objects: urldata}),
-      headers = {
-        'Content-Type': 'application/json',
-        'Content-Length': urls.length
-      },
-      options = {
-        host: 'url-caster.appspot.com',
-        port: 80,
-        path: '/resolve-scan',
-        method: 'POST',
-        headers: headers
-  };
-  
-  function urlOnly() {
-    var data = JSON.parse(urls);
-    for (var i in data.objects) {
-      console.log(chalk.underline.bgBlue(' ' + data.objects[i].url + ' '));
-      console.log(data.objects[i].url);
-      
-      notifier.notify({
-        title: data.objects[i].url,
-        message: data.objects[i].url,
-        icon: './physicalweb.jpg',
-        sound: true,
-        wait: true,
-        open: data.objects[i].url
-      });
-    }
-  }  
-
-  var req = http.request(options, function(res) {
-    res.setEncoding('utf-8');
-
-    var responseString = '';
-
-    res.on('data', function(data) {
-      responseString += data;
-    });
-
-    res.on('end', function() {
-      try {
-        var response = JSON.parse(responseString);
-        for (var i in response.metadata) {
-          var data = response.metadata[i];
-          console.log(chalk.underline.bgBlue(' ' + data.title + ' '));
-          console.log(chalk.gray(data.description));
-          console.log(data.displayUrl);
-          console.log();
+module.exports = urldata => {
+  let urls = JSON.stringify({'objects': urldata}),
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': urls.length
+    },
+    options = {
+      'host': 'url-caster.appspot.com',
+      'port': 80,
+      'path': '/resolve-scan',
+      'method': 'POST',
+      'headers': headers
+    },
+    urlOnly = () => {
+      let data = JSON.parse(urls);
+      for (let i in data.objects) {
+        if (data.objects.hasOwnProperty(i)) {
+          console.log(chalk.underline.bgBlue(' ' + data.objects[i].url + ' '));
+          console.log(data.objects[i].url);
 
           notifier.notify({
-            title: data.title,
-            subtitle: data.displayUrl,
-            message: data.description,
-            icon: './physicalweb.jpg',
-            contentImage: data.icon,
-            sound: true,
-            wait: true,
-            open: data.url
+            'title': data.objects[i].url,
+            'message': data.objects[i].url,
+            'icon': './physicalweb.jpg',
+            'sound': true,
+            'wait': true,
+            'open': data.objects[i].url
           });
         }
       }
-      catch(e) {
-        console.log(e);
-        console.log();
-        urlOnly();
-      }
-    });
-  });
+    },
+    req = http.request(options, res => {
+      res.setEncoding('utf-8');
 
-  req.on('error', function(e) {
+      let responseString = '';
+
+      res.on('data', data => {
+        responseString += data;
+      });
+
+      res.on('end', () => {
+        try {
+          let response = JSON.parse(responseString);
+          for (let i in response.metadata) {
+            if (response.metadata.hasOwnProperty(i)) {
+              let data = response.metadata[i];
+              console.log(chalk.underline.bgBlue(' ' + data.title + ' '));
+              console.log(chalk.gray(data.description));
+              console.log(data.displayUrl);
+              console.log();
+
+              notifier.notify({
+                'title': data.title,
+                'subtitle': data.displayUrl,
+                'message': data.description,
+                'icon': './physicalweb.jpg',
+                'contentImage': data.icon,
+                'sound': true,
+                'wait': true,
+                'open': data.url
+              });
+            }
+          }
+        } catch (e) {
+          console.log(e);
+          console.log();
+          urlOnly();
+        }
+      });
+    });
+
+  req.on('error', e => {
     console.log(e);
     console.log();
     urlOnly();
@@ -80,4 +82,4 @@ module.exports = function(urldata) {
 
   req.write(urls);
   req.end();
-}
+};
